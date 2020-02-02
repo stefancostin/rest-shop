29e2978e2299d4a6e52767c25dc70223d4bb61b2
stefancostin <stefanfcostin@gmail.com>
2020-02-02 10:42:39 +0200

    Add logging and error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const morgan = require('morgan');
+
+const productRoutes = require('./src/api/routes/products');
+const orderRoutes = require('./src/api/routes/orders');
 
 const app = express();
 
@@ -10,10 +12,32 @@ const app = express();
 //     message: 'It works!'
 //   });
 // });
+app.use(morgan('dev'));
 
 // Middleware
 // Format: app.use(FILTER, ROUTES)
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+/**
+ * Error Handling - Creating the 404 Error
+ */
+app.use((req, res, next) => {
+  const error = new Error('Resource not found.');
+  error.status = 404;
+  next(error);
+});
+
+/**
+ * Error Handling - Handling all errors
+ */
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
