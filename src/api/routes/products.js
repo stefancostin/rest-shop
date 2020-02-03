4db61e5f1412191b3df79152d5756e90ac10baee
stefancostin <stefanfcostin@gmail.com>
2020-02-03 23:10:35 +0200

    Add order section queries

--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -14,6 +14,7 @@ router.get('/', (req, res, next) => {
         count: docs.length,
         products: docs.map(doc => {
           return {
+            _id: doc._id,
             name: doc.name,
             price: doc.price,
             request: {
